c8dbc9d65c655bbea88fb22c94b92b14a2bd21ae
briac <briacsavoie@gmail.com>
2025-03-10 14:12:22 +0100

    Fix model loading issue in CI/CD

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 from scripts.newmodel import predict  # Fonction de prédiction
 
 
-model = tf.keras.models.load_model("models/image_audio_fusion_new_model_v2.keras")
+model = tf.keras.models.load_model("models/image_audio_fusion_new_model_v2.keras", compile=False)
+
 def test_prediction():
     result = predict(model, "data/images/cleaned/test_set/cats/cat.16.jpg", "data/audio/cleaned/train/cats/cat_1.wav")
     assert result in [0, 1, 2], "Erreur: Prédiction invalide"
