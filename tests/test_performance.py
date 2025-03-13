b383e04d603fd63ed6f2c8ebbb9288bf0cc65bdf
briac <briacsavoie@gmail.com>
2025-03-13 13:02:16 +0100

    mise a jours performace

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 print("chargement du model")
 
-model = tf.keras.models.load_model("models/image_audio_fusion_new_model.h5")
+model = tf.keras.models.load_model("models/fusion.h5")
 
 print("model chargé ...")
 
