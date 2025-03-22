218e9640d93c364be71c5f04cfd2047ddcf7a2f4
briac <briacsavoie@gmail.com>
2025-03-22 11:58:41 +0100

    Mise a jours des workflow performance 2

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,25 +1,55 @@
 # import sys
+# import os
+# import time
+# import memory_profiler
+# import tensorflow as tf
+
+# # Ajouter le chemin du dossier parent pour les imports
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# # Import des fonctions utilitaires pour reconstruire le modèle
+# from scripts.newmodel import predict, load_pretrained_models, build_fusion_model
+
+# print("Chargement des sous-modèles...")
+
+# # Reconstruction du modèle fusionné à partir des sous-modèles
+# image_feature_model, audio_feature_model = load_pretrained_models()
+# fusion_model = build_fusion_model(image_feature_model, audio_feature_model)
+
+# # Chargement des poids appris
+# fusion_model.load_weights("models/fusion.h5")
+
+# print("Modèle fusionné prêt pour l'inférence.")
+
+# def test_performance():
+#     start_time = time.time()
+#     predict(fusion_model, "data/images/cleaned/test_set/cats/cat.16.jpg", "data/audio/cleaned/train/cats/cat_1.wav")
+#     end_time = time.time()
+#     elapsed = end_time - start_time
+#     print("Temps d'inférence mesuré :", elapsed, "secondes")
+#     assert elapsed < 2.0, f"Erreur: Temps d'inférence trop long: {elapsed} secondes"
+
+import sys
 import os
 import time
 import pytest
 import memory_profiler
 import tensorflow as tf
-import sys 
 
-# Ajouter le chemin du dossier parent pour les imports
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# # Ajouter le chemin du dossier parent pour les imports
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-# Import des fonctions utilitaires pour reconstruire le modèle
-from scripts.newmodel import predict, load_pretrained_models, build_fusion_model
+# # Import des fonctions utilitaires pour reconstruire le modèle
+# from scripts.newmodel import predict, load_pretrained_models, build_fusion_model
 
-print("Chargement des sous-modèles...")
+# print("Chargement des sous-modèles...")
 
-# Reconstruction du modèle fusionné à partir des sous-modèles
-image_feature_model, audio_feature_model = load_pretrained_models()
-fusion_model = build_fusion_model(image_feature_model, audio_feature_model)
+# # Reconstruction du modèle fusionné à partir des sous-modèles
+# image_feature_model, audio_feature_model = load_pretrained_models()
+# fusion_model = build_fusion_model(image_feature_model, audio_feature_model)
 
-# Chargement des poids appris
-fusion_model.load_weights("models/fusion.h5")
+# # Chargement des poids appris
+# fusion_model.load_weights("models/fusion.h5")
 
 print("Modèle fusionné prêt pour l'inférence.")
 
