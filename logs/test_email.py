98c10448d187055f40094802990e5cbffe336c33
briac <briacsavoie@gmail.com>
2025-03-29 19:11:50 +0100

    fin

--- logs/test_email.py
+++
@@ -1,6 +0,0 @@
-from email_alert import send_error_email
-
-send_error_email("🚨 Test alerte MCO", "Ceci est un test d'envoi d'alerte par mail.")
-def test_force_error(client):
-    response = client.get("/force-error")
-    assert response.status_code == 500
